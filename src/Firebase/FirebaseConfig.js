3eca28ee621c142b3d3c0a4a0b8c8e6342c08657
asif883 <asifiqbal95863@gmail.com>
2024-11-19 22:38:04 +0600

    Basic setUp

---
+++ src/Firebase/FirebaseConfig.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCAbDEJWyEMB9N6dugpJInDbowD5tE0gTw",
+  authDomain: "tech-hub-7f6cd.firebaseapp.com",
+  projectId: "tech-hub-7f6cd",
+  storageBucket: "tech-hub-7f6cd.firebasestorage.app",
+  messagingSenderId: "503545701576",
+  appId: "1:503545701576:web:5b67a9d74c61a00e6631fd"
+};
+
+export const app = initializeApp(firebaseConfig);
---
+++ src/Routes/Router.jsx
@@ -0,0 +1,16 @@
+import Main from "../Layout/Main";
+import { createBrowserRouter } from "react-router-dom";
+import Home from "../Pages/Home";
+
+export const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Main></Main>,
+      children:[
+        {
+            path: '/',
+            element: <Home></Home>
+        }
+      ]
+    },
+  ]);
