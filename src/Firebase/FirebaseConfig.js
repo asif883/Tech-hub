2ca8e33ce8337f0c3c249e9e29721d7851b51685
asif883 <asifiqbal95863@gmail.com>
2024-11-19 22:46:50 +0600

    secure firebase key

--- src/Firebase/FirebaseConfig.js
+++ src/Firebase/FirebaseConfig.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCAbDEJWyEMB9N6dugpJInDbowD5tE0gTw",
-  authDomain: "tech-hub-7f6cd.firebaseapp.com",
-  projectId: "tech-hub-7f6cd",
-  storageBucket: "tech-hub-7f6cd.firebasestorage.app",
-  messagingSenderId: "503545701576",
-  appId: "1:503545701576:web:5b67a9d74c61a00e6631fd"
+  apiKey: import.meta.env.VITE_apiKey ,
+  authDomain: import.meta.env.VITE_authDomain ,
+  projectId: import.meta.env.VITE_projectId ,
+  storageBucket: import.meta.env.VITE_storageBucket ,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId ,
+  appId: import.meta.env.VITE_appId 
 };
 
 export const app = initializeApp(firebaseConfig);
