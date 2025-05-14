d3f60bbcf884e7cd22c86a955b0adb605256d7c5
asif883 <asifiqbal95863@gmail.com>
2025-05-14 11:26:01 +0600

    change color as dark mode

--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -6,6 +6,7 @@ import { useEffect, useState } from "react";
 import { FaCartPlus } from "react-icons/fa6";
 import { FaHeart, FaRegHeart } from "react-icons/fa";
 import { MdOutlineShoppingCart } from "react-icons/md";
+import useAuth from "../Hooks/useAuth";
 
 
 const Shop = () => {
@@ -19,7 +20,7 @@ const Shop = () => {
     
         const [ newBrand , setNewBrand] = useState([])
         const [ newCategory , setNewCategory] = useState([])
-    
+        const { isDarkMode } = useAuth()
         
          
         useEffect(()=>{
@@ -51,7 +52,7 @@ const Shop = () => {
         <div className="container mx-auto mt-10">
              <div className="grid gap-5 grid-cols-1 md:grid-cols-3 lg:grid-cols-4">
                  {/* left side */}
-                    <div className="border p-5">
+                    <div className={`border p-5 ${isDarkMode ? 'border-gray-700' : 'border-gray-200'}`}>
                         <FilterProduct
                          setBrand={setBrand}
                          setCategory={setCategory}
@@ -60,15 +61,16 @@ const Shop = () => {
                         />
                     </div>
                 {/* right side */}
-                    <div className="col-span-1 md:col-span-2 lg:col-span-3 border px-4 py-2">
-                        <div className="px-5 py-6 bg-gray-50 flex items-center justify-between">
+                    <div className={`col-span-1 md:col-span-2 lg:col-span-3 border px-4 py-2 ${isDarkMode ? 'border-gray-700' : 'border-gray-200'}`}>
+                        <div className={`px-5 py-6  flex items-center justify-between ${isDarkMode ? 'bg-gray-800' : 'bg-gray-50'}`}>
                             <p className="text-xl font-medium">Shop</p>
                             <Sort/>
                         </div>
 
                         <div className="grid gap-3 grid-cols-1 lg:grid-cols-2  pt-5">
                             {
-                                products?.map((product) => <div className="border rounded p-1 hover:shadow-md" key={product?._id}>
+                                products?.map((product) => <div className={`border rounded p-1 hover:shadow-md
+                                ${isDarkMode ? 'border-gray-600' : 'border-gray-200'}`} key={product?._id}>
                                     <div className="flex items-center gap-3">
                                         <img className="object-cover w-32 md:w-48 h-32 md:h-40" src={product?.imageURL} alt="" />
                                         <div className="space-y-1 relative w-full">
